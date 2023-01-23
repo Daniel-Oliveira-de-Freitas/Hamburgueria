50d2992516385e7fb736b239fed42036d37e2f30
2010101289 <danielodf2.aluno@unipampa.edu.br>
2023-01-23 17:17:56 -0300

    começando pagHamburgueria

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="text-align:center">
+    <h1>Olá Vue!!</h1>
+    <h2 style="color:blue">Meu nome é {{ nome }} e trabalho como {{ profissao }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Daniel",
+      profissao: 'Programador'
+    };
+  },
+};
+</script>
